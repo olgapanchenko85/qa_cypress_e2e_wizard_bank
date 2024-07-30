f73e682091cfbc31b3dce415c10a1bff02770514
Olha Panchenko <panchenko-85@ukr.net>
2024-07-30 13:24:07 +0300

    Tasks

--- cypress/e2e/hermioneAccount.cy.js
+++ cypress/e2e/hermioneAccount.cy.js
@@ -4,19 +4,23 @@ import { faker } from '@faker-js/faker';
 describe('Bank app', () => {
   const depositAmount = `${faker.number.int({ min: 500, max: 1000 })}`;
   const withdrawAmount = `${faker.number.int({ min: 50, max: 500 })}`;
-  const balance = depositAmount - withdrawAmount;
-  const user = 'Harry Potter';
-  const accountNumber = '1004';
+  const user = 'Hermoine Granger';
+  const accountNumber = '1001';
 
   before(() => {
     cy.visit('/');
   });
 
   it('should provide the ability to work with bank account', () => {
+    // Click Customer Login
     cy.contains('.btn', 'Customer Login').click();
+
+    // Select Harry Potter from dropdown
     cy.get('[name="userSelect"]').select(user);
+
     cy.contains('.btn', 'Login').click();
 
+    // Assert Account Number, Balance, and Currency
     cy.contains('[ng-hide="noAccount"]', 'Account Number')
       .contains('strong', accountNumber)
       .should('be.visible');
@@ -27,25 +31,56 @@ describe('Bank app', () => {
       .should('be.visible');
 
     cy.get('[ng-click="deposit()"]').click();
+
+    // Type deposit value and click Deposit
     cy.get('[placeholder="amount"]').type(depositAmount);
     cy.contains('[type="submit"]', 'Deposit').click();
 
+    // Assert success message and updated balance
     cy.get('[ng-show="message"]')
       .should('contain', 'Deposit Successful');
     cy.contains('[ng-hide="noAccount"]', 'Balance')
-      .contains('strong', depositAmount)
       .should('be.visible');
 
+    // Click Withdrawal
     cy.get('[ng-click="withdrawl()"]').click();
     cy.contains('[type="submit"]', 'Withdraw')
       .should('be.visible');
+
+    // Type withdraw value and click Withdraw
     cy.get('[placeholder="amount"]').type(withdrawAmount);
     cy.contains('[type="submit"]', 'Withdraw').click();
 
+    // Assert success message and updated balance
     cy.get('[ng-show="message"]')
       .should('contain', 'Transaction successful');
     cy.contains('[ng-hide="noAccount"]', 'Balance')
-      .contains('strong', balance)
       .should('be.visible');
+
+    // eslint-disable-next-line cypress/no-unnecessary-waiting
+    cy.wait(1000);
+    cy.get('[ng-click="transactions()"]').click();
+
+    cy.get('#anchor0 > :nth-child(1)').should('be.visible');
+    cy.get('#start').type('2024-07-29T00:00');
+
+    cy.get('#anchor0 > :nth-child(2)').should('contain', depositAmount);
+    cy.get('#anchor0 > :nth-child(3)').should('contain', 'Credit');
+    cy.get('#anchor1 > :nth-child(2)').should('contain', withdrawAmount);
+    cy.get('#anchor1 > :nth-child(3)').should('contain', 'Debit');
+
+    cy.get('[ng-click="back()"]').click();
+
+    // Change Account number
+    cy.get('[name="accountSelect"]').select('1002');
+
+    cy.get('[ng-click="transactions()"]').click();
+
+    cy.get('tbody tr').should('not.exist');
+
+    cy.get('[ng-click="byebye()"]').click();
+
+    // Assert user is logged out
+    cy.get('[name="userSelect"]').should('contain', 'Your Name');
   });
 });
